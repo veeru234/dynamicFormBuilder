6cbd0dca82f538c448730add516db9ca1208ed08
Veeru masal <veerumasal@zapbuild.com>
2026-05-27 10:04:01 +0530

    Initial Commit

---
+++ DynamicFormBuilder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DynamicFormBuilder
+//
+//  Created by Veeru Masal on 27/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DynamicFormBuilder/DynamicFormBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFormBuilderApp.swift
+//  DynamicFormBuilder
+//
+//  Created by Veeru Masal on 27/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFormBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
